2eb9d18fa5c346041340457dcd55e9b4c70aca64
Adibab <adiba.du.bd@gmail.com>
2022-12-29 23:56:31 +0000

    commit tour project

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -1,11 +1,72 @@
 import React, { useState, useEffect } from 'react'
 import Loading from './Loading'
 import Tours from './Tours'
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
+
 const url = 'https://course-api.com/react-tours-project'
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const[loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+const deleteTour = (id) => {
+const newTourArray = tours.filter((tour)=> tour.id !== id )
+ setTours (newTourArray)
+  }
+  const refresh = () => {
+    fetchTours();
+  };
+
+ 
+const fetchTours = async () => {
+  setLoading(true)
+
+  try{
+    const response = await fetch(url);
+    const tours = await response.json();
+   setTours (tours);
+      setLoading(false);
+
+  } catch (error) {
+     setLoading(false) 
+     console.log(error)
+  }
+
+};
+
+  useEffect(() => {
+    fetchTours();
+    // fetch(`https://course-api.com/react-tours-project`)
+    // .then((res) => res.json())
+    // .then((tours) => console.log(tours))
+    // setLoading(true)
+  }, []);
+
+ 
+if (loading) {
+  return (
+    <main>
+      <Loading />
+    </main>
+  );
+}
+if ( tours.length === 0){
+  return (
+    <main>
+      <div className="title">
+        <h2>no tours left</h2>
+        <button className="btn" onClick={refresh}>
+          Refresh
+        </button>
+      </div>
+    </main>
+  );
+}
+
+  return (
+    <div>
+      {/* <h2>Our Tours</h2> */}
+      < Tours tours= {tours} deleteTour={deleteTour} />
+    </div>
+  );
 }
 
 export default App
--- 02-tours/setup/src/Tour.js
+++ 02-tours/setup/src/Tour.js
@@ -1,7 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-const Tour = () => {
-  return <h2>tour component</h2>;
+const Tour = ({ id, image, name, info, price, deleteTour }) => {
+   const [ readmore, setReadmore ] = useState(false)
+   const readmoreInfo =()=> {
+    setReadmore (!readmore)
+   }
+  return (
+    <article className="single-tour" key={id}>
+      <img src={image} alt={name} />
+      <footer>
+        <div className="tour-info">
+          <h4>{name}</h4>
+          <h4 className="tour-price">${price}</h4>
+        </div>
+        <p> {readmore ? info : `${info.substring(0, 150)}`}.... <button onClick={readmoreInfo} > {readmore ? "Show Less" : "Read More" } </button></p>
+        <button className="delete-btn" onClick={(() => deleteTour(id))}> Not Interested </button>
+      </footer>
+    </article>
+  );
 };
 
 export default Tour;
